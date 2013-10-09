f3f3fc0c9ec15c9da990b19ead495417507876a5
jinshan jia <jiajinshan2009@gmail.com>
2013-10-09 01:05:38 -0400

    Remove Duplicates from Sorted Array

---
+++ RemoveDuplicates.java
@@ -0,0 +1,23 @@
+/*
+* Name: Jinshan Jia
+* 10/09/2013
+* Email: jiajinshan2009@gmail.com
+* Problem: Remove Duplicates from Sorted Array
+*/
+
+public class RemoveDuplicates {
+    public int removeDuplicates(int[] A) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (A.length == 0) {
+            return 0;
+        }
+        int moveToIndex = 0;
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] != A[moveToIndex]) {
+                moveToIndex++;
+                A[moveToIndex] = A[i];
+            }
+        }
+        return moveToIndex + 1;
+    }
+}
