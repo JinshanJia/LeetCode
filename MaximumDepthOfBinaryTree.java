dc70206edce87d688b5d1de9f0b9e26cd1a844a1
jinshan jia <jiajinshan2009@gmail.com>
2013-10-13 14:40:13 -0400

    Maximum Depth of Binary Tree

---
+++ MaximumDepthOfBinaryTree.java
@@ -0,0 +1,33 @@
+/*
+* Name: Jinshan Jia
+* 10/13/2013
+* Email: jiajinshan2009@gmail.com
+* Problem: Maximum Depth of Binary Tree
+*/
+/*
+ * Given a binary tree, find its maximum depth.
+ * The maximum depth is the number of nodes along the 
+ * longest path from the root node down to the farthest leaf node.
+ */
+public class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        // Note: The Solution object is instantiated only once and is reused by
+        // each test case.
+        if (root == null) {
+            return 0;
+        }
+        
+        if (root.left != null && root.right != null) {
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            return (left > right) ? 
+                    left + 1 : right + 1;
+        }
+        
+        if (root.left == null) {
+            return maxDepth(root.right) + 1;
+        } else {
+            return maxDepth(root.left) + 1;
+        }
+    }
+}
